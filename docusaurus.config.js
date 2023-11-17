623146ef3f03309469b5860d854e2609fd62810c
Julio Jimenez <julio@julioj.com>
2023-11-17 16:44:16 -0500

    social card
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/bogu-social-card.png',
       navbar: {
         title: 'bogu',
         logo: {
