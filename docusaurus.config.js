a6da1e03919f0f9a128f27a255942dd6c6a9ba73
Julio Jimenez <julio@julioj.com>
2023-11-17 16:56:16 -0500

    it's a partial url
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/bogu-io/bogu-docs',
+            'https://github.com/bogu-io/bogu-docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/bogu-io/bogu-docs',
+            'https://github.com/bogu-io/bogu-docs/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
