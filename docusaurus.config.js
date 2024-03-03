3aa4e7612917cb3ff300f6bb55c50f08673ccde5
Julio Jimenez <julio@julioj.com>
2024-03-03 05:12:36 -0500

    remove blog
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,57 +4,55 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'bogu',
-  tagline: 'The Secret Scanner',
-  favicon: 'img/favicon.png',
+  title: "bogu",
+  tagline: "The Secret Scanner",
+  favicon: "img/favicon.png",
 
   // Set the production url of your site here
-  url: 'https://bogu.io',
+  url: "https://bogu.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   // organizationName: 'facebook', // Usually your GitHub org/user name.
   // projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/bogu-io/bogu-docs/tree/main/',
+          editUrl: "https://github.com/bogu-io/bogu-docs/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/bogu-io/bogu-docs/tree/main/',
+          editUrl: "https://github.com/bogu-io/bogu-docs/tree/main/",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -64,42 +62,42 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/bogu-social-card.png',
+      image: "img/bogu-social-card.png",
       navbar: {
-        title: 'bogu',
+        title: "bogu",
         logo: {
-          alt: 'bogu',
-          src: 'img/favicon.png',
+          alt: "bogu",
+          src: "img/favicon.png",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Docs',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Docs",
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/bogu-io/bogu',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/bogu-io/bogu",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Getting Started',
-                to: '/docs/intro',
+                label: "Getting Started",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               // {
               //   label: 'Stack Overflow',
@@ -110,21 +108,21 @@ const config = {
               //   href: 'https://discordapp.com/invite/docusaurus',
               // },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/lispdev',
+                label: "Twitter",
+                href: "https://twitter.com/lispdev",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/bogu-io/bogu',
+                label: "GitHub",
+                href: "https://github.com/bogu-io/bogu",
               },
             ],
           },
